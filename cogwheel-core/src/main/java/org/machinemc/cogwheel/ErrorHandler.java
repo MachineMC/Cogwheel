58387cfe95313d692d2c00551659ed8dce5b2d12
_tud <98935832+UnderscoreTud@users.noreply.github.com>
2023-12-02 17:31:15 +0300

    Add new ErrorHandler implementation for throwing exceptions

--- cogwheel-core/src/main/java/org/machinemc/cogwheel/ErrorHandler.java
+++ cogwheel-core/src/main/java/org/machinemc/cogwheel/ErrorHandler.java
@@ -3,10 +3,15 @@ package org.machinemc.cogwheel;
 import org.machinemc.cogwheel.serialization.SerializerContext;
 import org.machinemc.cogwheel.util.error.ErrorEntry;
 
+import java.util.function.Function;
+
 public interface ErrorHandler {
 
     ErrorHandler SUPPRESSING = (context, error) -> {};
     ErrorHandler NORMAL = new DefaultErrorHandler();
+    Function<Function<String, ? extends RuntimeException>, ErrorHandler> THROW_EXCEPTION = throwable -> (context, error) -> {
+        throw throwable.apply(error.type() + ": " + error.message());
+    };
 
     void handle(SerializerContext context, ErrorEntry error);
 
