3a00c25197ba49786483c3af90ea5adc4318fdfe
_tud <98935832+UnderscoreTud@users.noreply.github.com>
2024-01-02 13:33:30 +0300

    Bump cogwheel-core version to 1.2.0

--- cogwheel-core/build.gradle.kts
+++ cogwheel-core/build.gradle.kts
@@ -23,7 +23,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "org.machinemc"
             artifactId = "cogwheel-core"
-            version = "1.1.0"
+            version = "1.2.0"
             from(components["java"])
         }
     }
